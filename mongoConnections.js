7a70cbac2c6f60d09cd8bfdf92744c76cd8df45a
Stephan D <37081797+Stephan-MDD@users.noreply.github.com>
2019-05-27 01:19:34 +0200

    Delete mongoConnections.js

--- mongoConnections.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    pro: "mongodb+srv://user:pass@mycluster-z4ipm.mongodb.net/express-graphql-api?retryWrites=true",
-    dev: "mongodb+srv://user:pass@mycluster-z4ipm.mongodb.net/express-graphql-api--dev?retryWrites=true",
-    test: "mongodb+srv://user:pass@mycluster-z4ipm.mongodb.net/express-graphql-api--test?retryWrites=true",
-}
